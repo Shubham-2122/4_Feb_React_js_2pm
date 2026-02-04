5691c62c8c536d6b60f25484e3cc39d806ccac76
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-02-04 14:49:34 +0530

    js

---
+++ AdvanceJs/ex.js
@@ -0,0 +1,13 @@
+console.log("hello this External js")
+
+// console.log(1)
+
+document.writeln("Hello shubham")
+
+// bom : broswer object model
+
+// pop box 
+// alert("Pls feild a data")
+// confirm("Are you sure ? ")
+// prompt("5 + 5 ? ")
+// print()
